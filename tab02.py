f70587a80a5b3f499c4f6b32600da3d43cefa43c
ezmiller-su <ezrajmiller@hotmail.com>
2026-04-22 19:33:18 -0400

    streamlit ui

--- tab02.py
+++ tab02.py
@@ -4,19 +4,47 @@ client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 st.title('Generate Tailored CV')
 
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "select_skills",
+            "description": "Select the user's most relevant skills",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "Must include the city, state, and country , e.g. San Francisco, CA, USA",
+                    },
+                },
+                "required": ["location"],
+            },
+        }
+    }]
+
+
 if jd := st.text_area('Paste Job Description'):
-    st.session_state.url_response = client.chat.completions.create(
+    response = client.chat.completions.create(
         model='gpt-5-nano',
-        max_tokens = 1024,
         messages=[{"role": "user",
                 "content": [
-                    {"type": "text", "text": '''Generate a resume tailored to this particular job description:\n
+                    {"type": "text", "text": f'''Generate a resume tailored to this particular job description:\n
                         {jd}\n
                         '''}]
-                    }]
+                    }],
+        tools=tools,
+        tool_choice="auto",
+        stream=False,
     )
 
+    message = response.choices[0].message
+
+    if message.tool_calls:
+        tool_call = message.tool_calls[0]
+
     cv = []
-    if st.session_state.url_response:
-        st.pdf(cv)
-        st.download_button('Download CV', cv)
+    if response:
+        st.text(message.content)
+        #st.pdf(cv)
+        #st.download_button('Download CV', cv)
